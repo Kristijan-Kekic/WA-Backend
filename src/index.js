61cd0fe41a38d72c4d45653a7aca7f14ee8400f3
Kristijan-Kekic <kkekic@unipu.hr>
2021-06-06 11:12:40 +0200

    Backend inicijalizacija i spajanje s frontendom, test privremene baze za prikaz

--- src/index.js
+++ src/index.js
@@ -1,40 +1,16 @@
 import express from 'express';
 import cors from 'cors';
-import data from './data';
+import storage from './memory_storage';
 
 const app = express()
 const port = 3000
 
+app.use(cors())
+
 app.listen(port, () => console.log(`slusam na portu ${port}`))
 
-//korisnici
+console.log(storage)
 
-app.get('/korisnici', (req, res) => res.send(data.korisnici))
-app.get('/korisnici/:username', (req, res) => {
-    let username  = req.params.username
-    res.json(data.korisnici.filter((x) => x.username == username));
-});
-
-//profesori
-
-app.get('/profesori', (req, res) => res.send(data.profesori))
-app.get('/profesori/:prof_id', (req, res) => {
-    let prof_id  = req.params.prof_id
-    res.json(data.profesori.filter((x) => x.prof_id == prof_id));
-});
-
-//ocjene
-
-app.get('/ocjene', (req, res) => res.send(data.ocjene))
-app.get('/ocjene/:ocjena_id', (req, res) => {
-    let ocjena_id  = req.params.ocjena_id
-    res.json(data.ocjene.filter((x) => x.ocjena_id == ocjena_id));
-});
-
-//komentari
-
-app.get('/komentari', (req, res) => res.send(data.komentari))
-app.get('/komentari/:ocjena_id', (req, res) => {
-    let ocjena_id  = req.params.ocjena_id
-    res.json(data.komentari.filter((x) => x.ocjena_id == ocjena_id));
+app.get("/ocjene", (req, res) => {
+    res.json(storage.ocjene)
 });
---
+++ src/memory_storage.js
@@ -0,0 +1,18 @@
+let storage = {
+ ocjene: [
+    {
+     id: 1,
+     profesor: "Ivo Ivić",
+     ocjena: 5,
+     komentar: "Veoma odlično!"
+    },
+    {
+     id: 2,
+     profesor: "Marko Markić",
+     ocjena: 3,
+     komentar: "OK je!"
+    },
+ ]
+}
+
+export default storage;
